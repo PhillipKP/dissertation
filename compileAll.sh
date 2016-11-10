8b5e8cea78bf81d81474dd949d5d0f50ebe04186
Phillip K. Poon <ppoon@optics.arizona.edu>
2016-11-10 17:56:32 -0500

    Finished draft of forward model in Ch5 spectral unmixing

--- compileAll.sh
+++ compileAll.sh
@@ -1,10 +1,19 @@
 #!/bin/bash 
-
+# This is a shell script that deletes the current pdf and compiles a new one
+rm Dissertation.pdf
+sleep 2
 pdflatex Dissertation
-
+sleep 2
 pdflatex Dissertation
-
+sleep 2
 makeglossaries Dissertation
-
+sleep 2
 pdflatex Dissertation
-
+sleep 2
+pdflatex Dissertation
+sleep 2
+bibtex Dissertation
+sleep 2
+pdflatex Dissertation
+sleep 2
+open Dissertation.pdf
